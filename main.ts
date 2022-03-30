c3b27866851d0cf5f491cdff9bb7416cca26d492
egarciatom <101406426+egarciatom@users.noreply.github.com>
2022-03-30 14:22:35 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,15 @@
-
+let temperatura = 0
+music.setVolume(105)
+basic.forever(function () {
+	
+})
+basic.forever(function () {
+    temperatura = input.temperature()
+    basic.showNumber(input.temperature())
+    if (input.temperature() < 5) {
+        basic.showIcon(IconNames.Chessboard)
+    }
+    if (input.temperature() > 30) {
+        basic.showIcon(IconNames.Square)
+    }
+})
